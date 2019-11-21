40531b2b31431a44e10418ebb6b9f9f95823f249
GabeKlavans <32149200+gabeklavans@users.noreply.github.com>
2019-11-20 19:43:05 -0500

    version 1.0 done

---
+++ csv_to_input.py
@@ -0,0 +1,86 @@
+import re
+import csv
+import operator
+from pathlib import Path
+import numpy as np
+import sys
+
+# Converts a midi note number to an ascii character
+# Can only handle C1 - B7
+def midi_to_char(midi: int) -> str:
+    if midi < 24:
+        return "NOTE_TOO_LOW_AHHHH"
+    elif midi <= 38:
+        return chr(midi + 9)
+    elif midi <= 107:
+        return chr(midi + 19)
+    else:
+        return "NOTE_TOO_HIGH_AHHHH"
+
+# string = "1, 0, Time_signature, 3, 2, 24, 8\n\
+# 1, 0, Key_signature, -1, \"major\"\n\
+# 1, 0, Tempo, 642192\n\
+# 1, 480, Tempo, 617920\n\
+# 1, 1440, Tempo, 563380\n\
+# 1, 1920, Tempo, 533333\n\
+# 1, 2160, Tempo, 538117\n\
+# 1, 2200, Tempo, 524017\n"
+
+
+string = Path(
+    '/Users/cassandra/GitHub/CS200-Project/alb.txt').read_text(encoding='cp1252')
+
+# Remove track/file info
+smol = re.sub('.*(Start|End).*\n', '', string)
+# Remove metadata stuff
+smol = re.sub('.*(_t|SMPTE|Time|Key).*\n', '', smol)
+# Remove tempo markings
+smol = re.sub('.*(Tempo).*\n', '', smol)
+# Don't know what this does but we don't need it soooo
+smol = re.sub('.*(Program|Control).*\n', '', smol)
+# Normalize to one track
+smol = re.sub('\n\d', '\n1', smol)
+# Remove header
+smol = re.sub('.*(Header).*\n', '', smol)
+
+#print(smol)
+
+# Convert to an array values from the CSV
+data = []
+
+for line in smol.splitlines():
+    line = [v.strip() for v in line.split(',')]
+    data.append(line)
+
+# put the timings in a data stucture in the form [time, note, velocity]
+# velocities are used for either on or off cuz we want those spicy note lengths
+timings = [[int(row[1]), int(row[4]), int(row[5])] for row in data]
+
+# sort the timings by time, so all chords are fully kept together
+timings = np.array(timings)
+timings = np.sort(timings.view('i8,i8,i8'), order=['f0'], axis=0).view(np.int)
+
+# the final output string
+output = ""
+
+# initialize last time to the first event time
+last_time = timings[0][0]
+
+for line in timings:
+    time = line[0]
+    note_num = line[1]
+    velocity = line[2]
+
+    if (time - last_time) > 0:
+        diff = time - last_time
+        output += f' {diff} '
+    # TODO: Map the MIDI input number to the ASCII character i want
+    # getMap(note)
+    output += midi_to_char(note_num)
+    if velocity == 0:
+        output += "."
+    last_time = time
+
+
+np.set_printoptions(threshold=sys.maxsize)
+print(output)
