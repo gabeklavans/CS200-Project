ade1680fd3bf5283bd9b60743b91037316545eec
GabeKlavans <32149200+gabeklavans@users.noreply.github.com>
2019-11-20 20:59:19 -0500

    Output proc works but no triplets

--- output_to_csv.py
+++ output_to_csv.py
@@ -28,20 +28,29 @@ input_txt = Path(
 # split up string into space-separated values
 data = input_txt.split(' ')
 
-timings = []
+timings = [['0', '0', 'Header', '1', '1', '480'], ['1', '0', 'Start_track']]
 
 current_time = 0
 # loop through all characters and do processing
 for value in data:
     if is_number(value):
         current_time += int(value)
-    for i in range(len(value)):
-        # remove this statement for debugging
-        if char_to_midi(value[i]) < 0:
-            pass # skip it
-        elif value[i] == '.':
-            timings.append([current_time, char_to_midi(value[i+1]), 0])
-        else:
-            timings.append([current_time, char_to_midi(value[i]), 100])
+    else:
+        i = 0
+        while i < len(value):
+            # remove this statement for debugging
+            if char_to_midi(value[i]) < 0:
+                i += 1 # skip it
+            elif value[i] == '.':
+                timings.append(['1', str(current_time), 'Note_on_c', '0', str(char_to_midi(value[i+1])), '0'])
+                i += 2
+            else:
+                timings.append(['1', str(current_time), 'Note_on_c', '0', str(char_to_midi(value[i])), '100'])
+                i += 1
 
-print(timings)
+timings.append(['1', str(current_time), 'End_track'])
+timings.append(['0', '0', 'End_of_file'])
+
+output = '\n'.join([", ".join(line) for line in timings])
+
+print(output)
